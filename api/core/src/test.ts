d3e055180a02ab1f495bd2d20d6d4c975462f5fe
Lucas Garcia Nachtigall <69644385+lucas-nachtigall@users.noreply.github.com>
2022-09-19 10:40:25 -0300

    Update test.ts

--- api/core/src/test.ts
+++ api/core/src/test.ts
@@ -1,11 +1,12 @@
 import { API, RegisterOptions } from "lambda-api";
 
 module.exports = (api: API, opts: RegisterOptions) => {
-api.get("/status", async (req, res) => {
+
+    api.get("/status", async (req, res) => {
         try {
             console.log('/api/status - log');
             console.error('/api/status - error');
-            res.status(200).json({ bohr_api: true? });
+            res.status(200).json({ bohr_api: true });
         } catch (e) {
             console.error(e);
             res.status(500);
