6f4771404cd76d5decb0da48be0c589097e2b39b
Lucas Garcia Nachtigall <69644385+lucas-nachtigall@users.noreply.github.com>
2022-09-19 10:33:49 -0300

    Create test.ts

---
+++ api/core/src/test.ts
@@ -0,0 +1,14 @@
+import { API, RegisterOptions } from "lambda-api";
+
+module.exports = (api: API, opts: RegisterOptions) => {
+api.get("/status", async (req, res) => {
+        try {
+            console.log('/api/status - log');
+            console.error('/api/status - error');
+            res.status(200).json({ bohr_api: true? });
+        } catch (e) {
+            console.error(e);
+            res.status(500);
+        }
+    });
+};
